c4edef24b6f3fb8ca9502c8746d9a9cd50fe43bd
Stas Vishnevetskiy <stanislav.vishnevetskiy@elbit.com>
2019-03-11 17:26:16 +0200

    added eslint & prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from "react";
-import { hot } from 'react-hot-loader/root'
-import "./app.scss";
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+import './app.scss';
 
 const App = () => {
-  return (
-    <div className="full-screen">
-      <div>
-        <h1>
-          I 360
-        </h1>
-        <br />
-        <a
-          className="button-line"
-          href=""
-          target="_blank"
-        >
-          Know more now
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <div className="full-screen">
+            <div>
+                <h1>I 360</h1>
+                <br />
+                <a className="button-line" href="" target="_blank">
+                    Know more now
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default hot(App);
